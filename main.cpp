899b4ae67f941c4b23944f3f6d7d1651d7f7e3d9
Boon Jun <boon-jun1996@hotmail.com>
2019-04-08 13:43:07 +0800

    New Architecture

--- main.cpp
+++ main.cpp
@@ -19,8 +19,6 @@
 
 QueueHandle_t leftMotorQueue = NULL;
 QueueHandle_t rightMotorQueue = NULL;
-QueueHandle_t ledQueue = NULL;
-QueueHandle_t audioQueue = NULL;
 
 void audio2Task(void *p) {
 	TickType_t xLastWakeTime = xTaskGetTickCount();
@@ -32,28 +30,50 @@ void audio2Task(void *p) {
 
 void serialTask(void *p) {
 	char output = 0;
+	int move = 1, completed = 0;
 	TickType_t xLastWakeUpTime = 0;
-
+	TaskHandle_t stopLedHandler, moveLedHandler, babySharkHandler,
+			completeSoundHandler;
+	xTaskCreate(moveLedTask, "moveLed", STACK_SIZE, NULL, 2, &moveLedHandler);
+	xTaskCreate(stopLedTask, "stopLed", 50, NULL, 2, &stopLedHandler);
+	xTaskCreate(babySharkTask, "babyShark", STACK_SIZE, NULL, 1,
+			&babySharkHandler);
+	xTaskCreate(completeAudioTask, "complete", 50, NULL, 1,
+			&completeSoundHandler);
+	xTaskCreate(leftMotorTask, "left", STACK_SIZE, NULL, 4, NULL);
+	xTaskCreate(rightMotorTask, "right", STACK_SIZE, NULL, 4, NULL);
 	for (;;) {
 		if (Serial.available()) {
 			output = Serial.read();
-			if (output & B00100000) {
-				xQueueReset(ledQueue);
+			if (move != output & B00100000) {
+				move = output & B00100000;
+				if (move) {
+					vTaskSuspend(stopLedHandler);
+					vTaskResume(moveLedHandler);
+				} else {
+					vTaskSuspend(moveLedHandler);
+					vTaskResume(stopLedHandler);
+				}
 			}
-			if (output & B00010000) {
-				xTaskCreate(audio2Task, "left", STACK_SIZE, NULL, 5, NULL);
+			if (completed != output & B00010000) {
+				completed = output & B00010000;
+				if (completed) {
+					vTaskSuspend(babySharkTask);
+					vTaskResume(stopLedHandler);
+				} else {
+					vTaskSuspend(babySharkTask);
+					vTaskResume(completeSoundHandler);
+				}
 			}
-			xQueueSend(leftMotorQueue, &output, 1);
-			xQueueSend(rightMotorQueue, &output, 1);
-			xQueueSend(ledQueue, &output, 1);
-			xQueueSend(audioQueue, &output, 1);
+			xQueueOverwrite(leftMotorQueue, &output);
+			xQueueOverwrite(rightMotorQueue, &output);
 		}
 		vTaskDelayUntil(&xLastWakeUpTime, 50);
 	}
 }
 
 void leftMotorTask(void *p) {
-	int onPeriod = 0;
+	int offset = 0;
 	char output = 0;
 	int move = 0;
 	int direction = 0;
@@ -61,7 +81,8 @@ void leftMotorTask(void *p) {
 
 	for (;;) {
 		if (xQueueReceive(leftMotorQueue, &output, 0) == pdTRUE) {
-			onPeriod = (output & B00001111) - 7;
+			offset = (output & B00001111) - 7;
+			if (offset > 0) offset = 0;
 			move = output & B00100000;
 			direction = (output & B10000000);
 		}
@@ -74,22 +95,22 @@ void leftMotorTask(void *p) {
 			//bit 7 indicates forward turn for left wheel
 			digitalWrite(5, LOW);
 			digitalWrite(6, HIGH);
-			vTaskDelayUntil(&xLastWakeUpTime, 8 + onPeriod);
+			vTaskDelayUntil(&xLastWakeUpTime, 8 + offset);
 			digitalWrite(6, LOW);
-			vTaskDelayUntil(&xLastWakeUpTime, 20 - (8 + onPeriod));
+			vTaskDelayUntil(&xLastWakeUpTime, 20 - (8 + offset));
 		} else {
 			//bit 7 indicates backward turn for left wheel
 			digitalWrite(5, HIGH);
 			digitalWrite(6, LOW);
-			vTaskDelayUntil(&xLastWakeUpTime, 8 - onPeriod);
+			vTaskDelayUntil(&xLastWakeUpTime, 8 + offset);
 			digitalWrite(5, LOW);
-			vTaskDelayUntil(&xLastWakeUpTime, 20 - (8 - onPeriod));
+			vTaskDelayUntil(&xLastWakeUpTime, 20 - (8 + offset));
 		}
 	}
 }
 
 void rightMotorTask(void *p) {
-	int onPeriod = 0;
+	int offset = 0;
 	int direction = 0;
 	int move = 0;
 	char output;
@@ -97,7 +118,8 @@ void rightMotorTask(void *p) {
 
 	for (;;) {
 		if (xQueueReceive(rightMotorQueue, &output, 0) == pdTRUE) {
-			onPeriod = (output & B00001111) - 7;
+			offset = (output & B00001111) - 7;
+			if (offset < 0) offset = 0;
 			move = output & B00100000;
 			direction = (output & B01000000);
 		}
@@ -110,21 +132,21 @@ void rightMotorTask(void *p) {
 			//bit 6 indicates forward turn for right wheel
 			digitalWrite(11, HIGH);
 			digitalWrite(3, LOW);
-			vTaskDelayUntil(&xLastWakeUpTime, 8 - onPeriod);
+			vTaskDelayUntil(&xLastWakeUpTime, 8 - offset);
 			digitalWrite(11, LOW);
-			vTaskDelayUntil(&xLastWakeUpTime, 20 - (8 - onPeriod));
+			vTaskDelayUntil(&xLastWakeUpTime, 20 - (8 - offset));
 		} else {
 			//bit 6 indicates backward turn for right wheel
 			digitalWrite(11, LOW);
 			digitalWrite(3, HIGH);
-			vTaskDelayUntil(&xLastWakeUpTime, 8 + onPeriod);
+			vTaskDelayUntil(&xLastWakeUpTime, 8 - offset);
 			digitalWrite(3, LOW);
-			vTaskDelayUntil(&xLastWakeUpTime, 20 - (8 + onPeriod));
+			vTaskDelayUntil(&xLastWakeUpTime, 20 - (8 - offset));
 		}
 	}
 }
 
-void ledTask(void *p) {
+void stopLedTask(void *p) {
 
 	for (int x = 0; x < 2; x++) {
 		//First connection
@@ -139,75 +161,79 @@ void ledTask(void *p) {
 	}
 
 	int numberToDisplay = 1;
-	char output;
-	int move = 0;
-	TickType_t xLastWakeTime = xTaskGetTickCount();
-
 	for (;;) {
-		if (xQueueReceive(ledQueue, &output, 0) == pdTRUE) {
-			move = output & B00100000;
-		}
-		if (move) {
-			//one front led lighted up according to numberToDisplay
-			digitalWrite(LATCH_PIN, LOW);
-			shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, numberToDisplay);
-			digitalWrite(LATCH_PIN, HIGH);
+		digitalWrite(LATCH_PIN, LOW);
+		shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, 255);
+		digitalWrite(LATCH_PIN, HIGH);
 
-			//all rear led on for 500ms
-			digitalWrite(REAR_LED_PIN, HIGH);
-			vTaskDelayUntil(&xLastWakeTime, 500);
-
-			//updates next front led to light up
-			numberToDisplay =
-					(numberToDisplay == 128) ? 1 : numberToDisplay << 1;
-
-			//all rear led off for 500ms
-			digitalWrite(REAR_LED_PIN, LOW);
-			vTaskDelayUntil(&xLastWakeTime, 500);
-		} else {
-			//all front led lighted up
-			digitalWrite(LATCH_PIN, LOW);
-			shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, 255);
-			digitalWrite(LATCH_PIN, HIGH);
-
-			// all rear led on and off for 250ms each
-			digitalWrite(REAR_LED_PIN, HIGH);
-			vTaskDelayUntil(&xLastWakeTime, 250);
-			digitalWrite(REAR_LED_PIN, LOW);
-			vTaskDelayUntil(&xLastWakeTime, 250);
-		}
+		// all rear led on and off for 250ms each
+		digitalWrite(REAR_LED_PIN, HIGH);
+		vTaskDelay(250);
+		digitalWrite(REAR_LED_PIN, LOW);
+		vTaskDelay(250);
 	}
 }
 
-void audioTask(void *p) {
-	TickType_t xLastWakeTime = xTaskGetTickCount();
-	int isCompleted = 0;
-	int output = 0; //completion state of challenge
+void moveLedTask(void *p) {
+	int numberToDisplay = 1;
+
+	for (;;) {
+		//one front led lighted up according to numberToDisplay
+		digitalWrite(LATCH_PIN, LOW);
+		shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, numberToDisplay);
+		digitalWrite(LATCH_PIN, HIGH);
+
+		//all rear led on for 500ms
+		digitalWrite(REAR_LED_PIN, HIGH);
+		vTaskDelay(500);
+
+		//updates next front led to light up
+		numberToDisplay = (numberToDisplay == 128) ? 1 : numberToDisplay << 1;
+
+		//all rear led off for 500ms
+		digitalWrite(REAR_LED_PIN, LOW);
+		vTaskDelay(500);
+	}
+}
+
+void babySharkTask(void *p) {
+	for (int i = 0; i < 3; i++) {
+		//indicate first connection
+		tone(BUZZER_PIN, 330, 500);
+		vTaskDelay(600);
+	}
 	for (;;) {
 		for (int i = 0; i < 3; i++) {
 			tone(BUZZER_PIN, 294, 200);
-			vTaskDelayUntil(&xLastWakeTime, 250);
+			vTaskDelay(250);
 			tone(BUZZER_PIN, 330, 200);
-			vTaskDelayUntil(&xLastWakeTime, 300);
+			vTaskDelay(300);
 
 			for (int k = 0; k < 3; k++) {
 				tone(BUZZER_PIN, 392, 200);
-				vTaskDelayUntil(&xLastWakeTime, 300);
+				vTaskDelay(300);
 			}
 			for (int j = 0; j < 2; j++) {
 				tone(BUZZER_PIN, 392, 200);
-				vTaskDelayUntil(&xLastWakeTime, 210);
+				vTaskDelay(210);
 				tone(BUZZER_PIN, 392, 200);
-				vTaskDelayUntil(&xLastWakeTime, 300);
+				vTaskDelay(300);
 			}
-			vTaskDelayUntil(&xLastWakeTime, 75);
+			vTaskDelay(75);
 		}
 		tone(BUZZER_PIN, 392, 200);
-		vTaskDelayUntil(&xLastWakeTime, 300);
+		vTaskDelay(300);
 		tone(BUZZER_PIN, 392, 200);
-		vTaskDelayUntil(&xLastWakeTime, 300);
+		vTaskDelay(300);
 		tone(BUZZER_PIN, 349, 200);
-		vTaskDelayUntil(&xLastWakeTime, 2000);
+		vTaskDelay(2000);
+	}
+}
+
+void completeAudioTask(void *p) {
+	for (;;) {
+		tone(BUZZER_PIN, 392, 500);
+		vTaskDelay(600);
 	}
 }
 
@@ -216,12 +242,12 @@ void setup() {
 //right motor
 	pinMode(11, OUTPUT);
 	pinMode(3, OUTPUT);
-	rightMotorQueue = xQueueCreate(5, sizeof(char));
+	rightMotorQueue = xQueueCreate(1, sizeof(char));
 
 //left motor
 	pinMode(5, OUTPUT);
 	pinMode(6, OUTPUT);
-	leftMotorQueue = xQueueCreate(5, sizeof(char));
+	leftMotorQueue = xQueueCreate(1, sizeof(char));
 
 //shift register(green led)
 	pinMode(LATCH_PIN, OUTPUT);
@@ -233,25 +259,15 @@ void setup() {
 
 //red led
 	pinMode(REAR_LED_PIN, OUTPUT);
-	ledQueue = xQueueCreate(5, sizeof(char));
 
 //tone
-	audioQueue = xQueueCreate(5, sizeof(char));
+	pinMode(BUZZER_PIN, OUTPUT);
 }
 
 void loop() {
-	if (Serial.available()) {
-		xTaskCreate(leftMotorTask, "left", STACK_SIZE, NULL, 4, NULL);
-		xTaskCreate(rightMotorTask, "right", STACK_SIZE, NULL, 4, NULL);
-
-		xTaskCreate(serialTask, "serial", STACK_SIZE, NULL, 3, NULL);
-
-		xTaskCreate(ledTask, "led", STACK_SIZE, NULL, 2, NULL);
-
-		xTaskCreate(audioTask, "audio", STACK_SIZE, NULL, 1, NULL);
-
+	if (Serial) {
+		xTaskCreate(serialTask, "serial", STACK_SIZE, NULL, 5, NULL);
 		vTaskStartScheduler();
-		while (1)
-			;
+		while (1);
 	}
 }
